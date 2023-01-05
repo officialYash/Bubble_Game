eaed85253a7222a6b8050acc849737288519c82d
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-05 14:45:16 +0530

    Some Changed Logic Part on Game

--- script.js
+++ script.js
@@ -53,38 +53,45 @@ const alphabhetArray=[
   "Z",
 ];
 
+const bubblecolors=[
+  "purple",
+  "red",
+  "green",
+  "cyan",
+  "orange",
+  "pink",
+  "gray",
+]
+
+
 // GETTING BUBBLE FROM DOM
 
-let Bubbles;
+let Bubbles=[];
 
-// Creating the Game Loop
+let score=0;
 
-let StartTime = new Date().getTime();
-let frameRate = 1;
 
-// Game Loop For Running the Game
+let missBubble=0;
 
-const gameloop= () =>{
-    const currentTime = new Date().getTime();
-    const differenceTime = currentTime -StartTime;
-    if(differenceTime > 1000 / frameRate){
-        console.log("good Time");
-        StartTime = currentTime;
-    }
-    window.requestAnimationFrame(gameloop)
-};
 
-// Gameloop();
 
 // Function For Creating the Bubble
 
 const createbubble = () =>{
+    // getting a all the Random values for bubble
+    const randomAlphabet = Math.floor(Math.random() * alphabhetArray.length);
+    const randomColor =Math.floor(Math.random() * bubblecolors.length);
+    const randomTime=Math.floor(Math.random() * (30 - 10 + 1) + 10);
+    const randomLeft = Math.floor(Math.random() * (95 -5 + 1) + 5);
+    // Creating the new Bubble with random Values
     const mydiv = document.createElement("div");
     mydiv.style.width="50px";
     mydiv.style.height="50px";
     mydiv.style.bottom="0px";
-    mydiv.innerText=alphabhetArray[0];
-    mydiv.style.backgroundColor="red";
+    mydiv.innerText =alphabhetArray[randomAlphabet];
+    mydiv.style.backgroundColor=bubblecolors[randomColor];
+    mydiv.style.style["-webkit-animation-duration"] =`${randomTime}s`;
+    mydiv.style.left=`${randomLeft}%`;
     mydiv.style.boxShadow=
     "inset -5px -5px 10px rgba(0, 0, 0, 1),1px 1px 1px white,-1px -1px 1px white";
     mydiv.classList.add("bubble");
@@ -95,22 +102,55 @@ const createbubble = () =>{
     Bubbles=document.querySelectorAll(".bubble");
     Bubbles=Array.from(Bubbles);
 
+    // for deleting the bubble on animation end (bubble reach on top)
+    mydiv.addEventListener("webkitAnimationEnd", () =>{
+        document.body.removeChild(mydiv);
+        missBubble+=1;
+        console.log(missBubble);
+    })
+
 };
-createbubble();
 
-// Function for popping up the bubble
 
-const burstBubble = (event)=>{
-    const key =event.key;
-    Bubbles.map((element, index)=>{
-        if(element.innerText === key){
-            deleteIndex=index;
-            document.body.removeChild(Bubbles[index]);
+// Function for popping up the bubble if key matches
+
+    const burstBubble =(event)=>{
+        const key =event.key;
+        for(let i=0; i< Bubbles.length ;i++ ){
+            if(Bubbles[i].innerText === key){
+                // popping the bubble
+                document.body.removeChild(Bubbles[i]);
+            }
+
+            // Updating the bubble list after popping the bubble
             Bubbles=document.querySelectorAll(".bubble");
             Bubbles=Array.from(Bubbles);
+            break; 
         }
-    });
-};
 
-// function for adding the window event listner
+    };
+   
+    // creating the game Loop
+
+    let startTime = new Date().getTime();
+    let frameRate = 0.5;
+
+    // game loop for running the game 
+
+    const gameloop= () =>{
+        const currentTime=new Date().getTime();
+        const Difference = currentTime -startTime;
+        if(Difference >1000 / frameRate){
+            startTime=currentTime;
+            createbubble();
+        }
+        window.requestAnimationFrame(gameloop);
+    };
+
+
+
+
+
+
+// function for adding the window event listner for user key input
 window.addEventListener("keyup", burstBubble);
